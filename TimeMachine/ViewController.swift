a971d1b1f877a4f865c70795b0edce5fd6de1a4c
abwayland <abwayland@gmail.com>
2016-02-29 13:55:22 -0500

    Initial Commit

---
+++ TimeMachine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeMachine
+//
+//  Created by Adam Wayland on 2/29/16.
+//  Copyright © 2016 Adam Wayland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
